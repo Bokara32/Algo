7ce909062a0c1d3096c8d9c3a109044555ca3dc6
Julien Bugnicourt <bugnicourtj@gmail.com>
2024-11-18 10:20:00 +0100

    Maj1

--- Exercice Michael/Exercices d'introduction/Partie 3/Exercice3.2/src/App.java
+++ Exercice Michael/Exercices d'introduction/Partie 3/Exercice3.2/src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
+      /* Ouverture du scanner */
+
+      Scanner scan= new Scanner(System.in);
+
+      /* Déclaration de la valeur Vide */
+
+      int nombreS = 0;
+
+      /* Déclaration des instructions de boucle */
+      
+       do { System.out.print(" Saisissez une Valeur entre 10 et 20 \n \n ");
+
+          nombreS = scan.nextInt(); 
+
+          /* Condition est affichage */
+
+          if ( nombreS< 10){ System.out.print(" Chiffre saisi inférieur à 10 Saisissez un chifre plus grand !");}
+    
+          else if (nombreS > 20){System.out.print( " Chiffre saisi supérieur à 20 Saisissez un chiffre plus petit ! ");}
+    
+          else if (nombreS <17 && nombreS >=10){System.out.print(" Le chiffre à trouver est plus grand ! "); }
+    
+          else if (nombreS > 17 && nombreS <=20){ System.out.print(" Le Chiffre à trouver est plus petit ! ");} 
+
+          /* Condition de la boucle */
+
+        } while (nombreS != 17); 
+
+          if (nombreS == 17){ System.out.print(" Yes, you did it !! ");}
+
+          /* Fermeture du Scanner */
+
+          scan.close();
+
+
     }
 }
